804c8163d35e96d1878966982bbc8f2c5f14059a
Casapr <279397942@qq.com>
2020-05-26 18:01:21 +0800

    init projects

---
+++ ace-server-projects/k8s-add-private-image-hub.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+## 添加私有镜像仓库
+kubectl create secret docker-registry registry-secret \
+--docker-server=nexus3.ace.com:8082 \
+--docker-username=nexus \
+--docker-password=ht9EhOscUesy5aSkwluBFqdK4FyqkdTB \
+--docker-email=279397942@qq.com \
+-n ace
